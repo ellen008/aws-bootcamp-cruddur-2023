76a90f8cd9e673c0d890b3b11d5766fb323aec3d
ellen008 <photon008@gmail.com>
2023-03-12 06:07:21 +0000

    identation again

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -48,5 +48,5 @@ class HomeActivities:
         'replies': []
       }
       ]
-     span.set_attribute("app.results_length", len(results) ) 
-     return results
+    span.set_attribute("app.results_length", len(results) ) 
+    return results
