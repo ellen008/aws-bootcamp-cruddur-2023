0e7982559a87dbbb554ea9651a40297aa5a1df58
ellen008 <44596756+ellen008@users.noreply.github.com>
2023-03-28 04:43:13 +0000

    fixed identation

--- backend-flask/services/show_activity.py
+++ backend-flask/services/show_activity.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 class ShowActivities:
-    def run(activity_uuid):
+    def run(self,activity_uuid):
         now = datetime.now(timezone.utc).astimezone()
         results = [{
           'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
