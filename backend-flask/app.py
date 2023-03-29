230ff42c83c86c2679af0ffec5f3ca4ea4054a26
ellen008 <44596756+ellen008@users.noreply.github.com>
2023-03-29 03:44:00 +0000

    updated CORS

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
+from flask import got_request_exception
 from flask_cors import CORS, cross_origin
+import sys
 import os
 
 from services.home_activities import *
@@ -35,7 +37,7 @@ from time import strftime
 #Rollbar
 import rollbar
 import rollbar.contrib.flask
-from flask import got_request_exception
+
 
 
 #Honeycomb init
@@ -82,8 +84,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -150,6 +152,8 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+    app.logger.debug("AUTH_HEADER----")
+    app.logger.debug(request.headers.get('Authorization'))
     data = HomeActivities.run()
     return data, 200
 
